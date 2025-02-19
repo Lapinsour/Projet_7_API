0dee0aeab2a8cde4bb084e39d2a7c292289ef58e
Pierre Garrigues <141630659+Lapinsour@users.noreply.github.com>
2025-02-19 13:00:30 +0100

    Update app.py
    Modification de la host

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+
+
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -15,4 +17,4 @@ def predict():
     return jsonify({"error": "Invalid input"}), 400
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="127.0.0.1", port=5000)
