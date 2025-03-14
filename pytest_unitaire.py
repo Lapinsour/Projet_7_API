52095ac464c753fe50c1aaa836bc1f1062b1092a
Pierre Garrigues <141630659+Lapinsour@users.noreply.github.com>
2025-03-14 12:06:33 +0100

    Update pytest_unitaire.py

--- pytest_unitaire.py
+++ pytest_unitaire.py
@@ -8,6 +8,7 @@ GITHUB_CSV_URL = "https://raw.githubusercontent.com/Lapinsour/Projet_7_API/refs/
 
 def load_random_data():    
     df = pd.read_csv(GITHUB_CSV_URL)
+    df = df.drop('TARGET', axis=1)
     # Sélectionner une ligne aléatoire et convertir en dict
     return df.sample(n=1).iloc[0].to_dict()
 
