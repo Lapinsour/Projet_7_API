bb61676f1007dabccf99fe2694c11d59971aa2c4
Pierre Garrigues <141630659+Lapinsour@users.noreply.github.com>
2025-03-14 11:53:32 +0100

    Create pytest_unitaire.py

---
+++ pytest_unitaire.py
@@ -0,0 +1,17 @@
+import pytest
+import pandas as pd
+import requests
+from app import predict_model
+
+# URL brute du fichier CSV sur GitHub 
+GITHUB_CSV_URL = "https://github.com/Lapinsour/Projet_7_API/blob/main/df_sample.csv"
+
+def load_random_data():    
+    df = pd.read_csv(GITHUB_CSV_URL)
+    # Sélectionner une ligne aléatoire et convertir en dict
+    return df.sample(n=1).iloc[0].to_dict()
+
+def test_predict_model():
+    data = load_random_data()  # Récupérer une ligne aléatoire du CSV
+    result = predict_model(data)
+    assert 0 <= result <= 1
